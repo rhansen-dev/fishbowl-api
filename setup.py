1a875fb47faea48934b53683a6d0e8b0acf76ed9
Chris Beaven <smileychris@gmail.com>
2015-12-01 12:10:10 +1300

    Move everything to a fishbowl package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fishbowl-api',
+    version='0.1',
+    description='Fishbowl API',
+    url='http://github.com/SmileyChris/fishbowl-api',
+    author='Chris Beaven',
+    author_email='smileychris@gmail.com',
+    license='MIT',
+    packages=['fishbowl'],
+)
