671c3449370ded056f6b0b5ee4d7f851c5203223
Chris Beaven <smileychris@gmail.com>
2015-12-01 14:57:17 +1300

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='fishbowl-api',
     version='0.1',
     description='Fishbowl API',
+    long_description=long_description,
     url='http://github.com/SmileyChris/fishbowl-api',
     author='Chris Beaven',
     author_email='smileychris@gmail.com',
