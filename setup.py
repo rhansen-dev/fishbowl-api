e2ca1f914b8c9125a49df5a2633135cc301ab8d5
Chris Beaven <smileychris@gmail.com>
2015-12-01 14:58:03 +1300

    Let's start the version at 1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='fishbowl-api',
-    version='0.1',
+    version='1.0',
     description='Fishbowl API',
     long_description=long_description,
     url='http://github.com/SmileyChris/fishbowl-api',
